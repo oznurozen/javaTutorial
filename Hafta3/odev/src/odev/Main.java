76f7997aa22846ed75b28fd4a5df60418ca2120c
oznurozen <oznurozen67@gmail.com>
2021-06-25 21:53:01 +0300

    ucuncu hafta ders notlari

--- Hafta3/odev/src/odev/Main.java
+++ Hafta3/odev/src/odev/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	         
 	      }
 		 
-		 System.out.print("-----------");
+		 System.out.println("-----------");
 		 
 		 for(int i=1; i<=10; i++){
 			  System.out.println();
